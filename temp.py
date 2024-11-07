b16b79d6399537715900b087355e3927166bdc92
Your Name <you@example.com>
2024-11-07 16:25:06 +0530

    sdfdsf

--- temp.py
+++ temp.py
@@ -1,17 +1,6 @@
-from bs4 import BeautifulSoup
-from googletrans import Translator
-
-def translate_html_content(html_content, target_language='hi'):
-    soup = BeautifulSoup(html_content, 'html.parser')
-    translator = Translator()
-
-    # Loop through all text nodes
-    for text_node in soup.find_all(string=True):
-        if text_node.strip():  # Only translate non-empty text
-            translated_text = translator.translate(text_node, dest=target_language).text
-            text_node.replace_with(translated_text)
-
-    # Return the translated HTML content
-    return str(soup)
-
-
+def create_post(request, folder_id):
+    folder = get_object_or_404(Folder, id=folder_id)
+    if request.method == 'POST':
+        print(request.POST)
+        return HttpResponse('form submitted')
+    return render(request, 'create_post.html')
